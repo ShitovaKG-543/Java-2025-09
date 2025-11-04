7696102cafe4262625338d6ee5f9a8077c51a9b4
ShitovaKG-543 <kshitova@inbox.ru>
2025-11-04 16:05:36 +0300

    hw04-gc - Выполнено исследование исходного кода на время выполнения при разных размерах хипа. Произведена его оптимизация: Класс Integer заменен на использование примитивного типа int, вынесены неизменные вычисления из цикла. Также произведено исследование оптимизированного кода на время выполнения при разных размерах хипа.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ rootProject.name = "Java-2025-09"
 include("hw01-gradle")
 include("hw02-generics")
 include("hw03-test-framework")
+include("hw04-gc")
 
 pluginManagement {
     val jgitver: String by settings
